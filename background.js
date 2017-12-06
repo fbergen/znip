3a0e2f3f6d410acdee1f42d9639b6ff7e5fd3de0
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2017-12-06 15:28:09 +0100

    open in next tab instead of last tab

--- background.js
+++ background.js
@@ -1,22 +1,20 @@
 chrome.browserAction.onClicked.addListener(function() {
   var page = chrome.extension.getBackgroundPage();
   chrome.tabs.insertCSS(null, {file: "content.css"}, function() {
-  	chrome.tabs.insertCSS(null, {file: "vendor/Jcrop.min.css"}, function() {
-    	chrome.tabs.executeScript(null, {file: "vendor/jquery-3.2.1.min.js"}, function() {
-    	  chrome.tabs.executeScript(null, {file: "vendor/Jcrop.min.js"}, function() {
-    	      chrome.tabs.executeScript(null, {file: "content_script.js"}, function() {
-    	    });
-    	  });
-    	});
-		});
+    chrome.tabs.insertCSS(null, {file: "vendor/Jcrop.min.css"}, function() {
+      chrome.tabs.executeScript(null, {file: "vendor/jquery-3.2.1.min.js"}, function() {
+        chrome.tabs.executeScript(null, {file: "vendor/Jcrop.min.js"}, function() {
+            chrome.tabs.executeScript(null, {file: "content_script.js"}, function() {
+          });
+        });
+      });
+    });
   });
 });
 
 chrome.runtime.onMessage.addListener(function(req, sender) {
   chrome.tabs.captureVisibleTab(function(img) {
     cropImage(img, req.selection, req.scroll, function(cropped) {
-      var viewTabUrl = chrome.extension.getURL('edit.html?id=' + 
-																						   Math.random().toString(36).substr(2, 9));
       var targetId = null;
       chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
         if (tabId != targetId || changedProps.status != "complete")
@@ -24,16 +22,23 @@ chrome.runtime.onMessage.addListener(function(req, sender) {
   
         chrome.tabs.onUpdated.removeListener(listener);
   
-				views = chrome.extension.getViews({type: 'tab', tabId: tabId})
-				console.log(views)
-				if (views.length >= 1) {
+        views = chrome.extension.getViews({type: 'tab', tabId: tabId})
+        console.log(views)
+        if (views.length >= 1) {
             views[0].setScreenshotUrl(1, cropped);
-            views[0].setScreenshotUrl(2, img);
-				}
+        }
       });
   
-      chrome.tabs.create({url: viewTabUrl}, function(tab) {
-        targetId = tab.id;
+      var createProperties = {}
+      createProperties.url = chrome.extension.getURL('edit.html?id=' + Math.random().toString(36).substr(2, 9));
+      chrome.tabs.query({active: true}, function(tabs) {
+        if (tabs.length > 0 && tabs[0].index) {
+          console.log(tabs[0].index)
+          createProperties.index = tabs[0].index + 1
+        }
+        chrome.tabs.create(createProperties, function(tab) {
+          targetId = tab.id;
+        });
       });
     });
   });
