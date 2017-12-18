72abd20d9fabcb1e9d1d5741b0c0dcd7764f641c
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2017-12-18 09:03:30 +0100

    Implement arrows

--- shapes.js
+++ shapes.js
@@ -59,3 +59,56 @@ Shape.Ellipse.prototype.draw = function() {
   ctx.stroke()
 };
 
+Shape.Arrow = function Arrow(options) {
+  this.strokeStyle = options.strokeStyle;
+  this.canvas = options.canvas;
+
+  this.lineWidth = options.lineWidth;
+  this.endPos = options.startPos
+  this.startPos = options.startPos
+};
+
+Shape.Arrow.prototype.addPos = function(pos) {
+  this.endPos = pos
+};
+
+Shape.Arrow.prototype.draw = function() {
+  ctx = this.canvas.getContext('2d');
+  ctx.strokeStyle = this.strokeStyle
+  ctx.lineWidth = this.lineWidth
+  ctx.beginPath();
+
+  s = this.startPos;
+  e = this.endPos;
+  ctx.moveTo(s[0], s[1])
+  ctx.lineTo(e[0], e[1])
+  ctx.stroke()
+
+  arr = arrowhead(s, e)
+  ctx.moveTo(e[0], e[1])
+  ctx.lineTo(arr[0][0], arr[0][1])
+
+  ctx.moveTo(e[0], e[1])
+  ctx.lineTo(arr[1][0], arr[1][1])
+  ctx.stroke()
+};
+
+
+function length(a, b) {
+  return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2))
+}
+
+function arrowhead(a, b) {
+  h = 20
+  w = 10
+  u = [(b[0] - a[0])/length(a,b), (b[1] - a[1])/length(a,b)]
+  v = [-u[1], u[0]]
+
+  v1 = [b[0] - h*u[0] + w*v[0],
+        b[1] - h*u[1] + w*v[1]]
+  v2 = [b[0] - h*u[0] - w*v[0],
+        b[1] - h*u[1] - w*v[1]]
+  return [v1, v2]
+}
+
+
